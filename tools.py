c0076a2dd22737db84ee8db16b22db2e4b8811a4
gogamid <imron.gamidli@lt.hs-fulda.de>
2021-07-02 16:29:30 +0200

    clean

--- tools.py
+++ tools.py
@@ -42,8 +42,6 @@ if __name__ == '__main__':
     elif action == "triangle":
         # Polygon can be clockwise or counter-clockwise
         polygon = [(-3,1), (3, 1), (2, -2.5), (6.5, -1.5), (8,2.5), (3,6), (-1.5,3),(-3,5)]
-        # print(tripy._is)
-        polygon.reverse()
         # polygon = [(1,7), (3, 2), (7, 1), (12,4), (9,6), (8,9), (6,5)]
         triangles = tripy.earclip(polygon)
         for i in triangles: 
